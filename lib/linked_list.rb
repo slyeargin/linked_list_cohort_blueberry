bce1ea3dbd04157af6c015c9537ecc862d352f5e
Eliza Brock <eliza@elizabrocksoftware.com>
2014-07-11 11:27:39 -0500

    Initial implementation of push and get

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,25 @@
 class LinkedList
 
+  def push(value)
+    lli = LinkedListItem.new(value)
+    if @first_item
+
+      current_item = @first_item
+      until current_item.last?
+        current_item = current_item.next_item
+      end
+      current_item.next_item = lli
+
+    else
+      @first_item = lli
+    end
+  end
+
+  def get(index)
+    current_item = @first_item
+    index.times do
+      current_item = current_item.next_item
+    end
+    current_item.payload
+  end
 end
