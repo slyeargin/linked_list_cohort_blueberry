0c543997d4acf749425a12b8da03d0ba8972aab8
Eliza Brock <eliza@elizabrocksoftware.com>
2014-07-11 10:24:58 -0500

    Implement LinkedListItem, with expanded comparisons.

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,34 @@
 class LinkedListItem
   include Comparable
+
+  attr_reader :next_item
+  attr_reader :payload
+
+  def initialize(value)
+    @payload = value
+  end
+
+  def next_item=(other_item)
+    raise ArgumentError if other_item === self
+    @next_item = other_item
+  end
+
+  def last?
+    @next_item.nil?
+  end
+
+  def <=>(other_item)
+    if self.payload.class == other_item.payload.class
+      self.payload <=> other_item.payload
+    else
+      precedence = [ Fixnum, String, Symbol ]
+      left = precedence.index(self.payload.class)
+      right = precedence.index(other_item.payload.class)
+      left <=> right
+    end
+  end
+
+  def ===(other_item)
+    self.equal? other_item
+  end
 end
