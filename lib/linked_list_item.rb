bf0c13d6f82d6e2b8d2a149bfa0897f2a4098d76
Samantha Yeargin <slyeargin@gmail.com>
2014-07-10 22:45:33 -0500

    EOD - linked_list_item

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,35 @@
 class LinkedListItem
   include Comparable
+
+  attr_reader :payload
+  attr_reader :next_item
+
+  def initialize(value)
+    @payload = value
+  end
+
+  def next_item=(other_item)
+    raise ArgumentError if other_item == self
+    @next_item = other_item
+  end
+
+  def last?
+    @next_item.nil?
+  end
+
+  def <=>(item)
+    if self.payload.class == item.payload.class
+      self.payload <=> item.payload
+    else
+      precedence = [Fixnum, String, Symbol]
+      self_index = precedence.index(self.payload.class)
+      item_index = precedence.index(item.payload.class)
+      self_index <=> item_index
+    end
+  end
+
+  def ===(item)
+    self.equal? item
+  end
+
 end
